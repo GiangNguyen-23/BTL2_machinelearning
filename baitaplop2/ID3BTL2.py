11ed22c5dd4b4f343d89f254750b5e260d8d492b
Giang Nguyễn <125534820+GiangNguyen-23@users.noreply.github.com>
2023-10-12 19:40:36 +0700

    Add files via upload

---
+++ baitaplop2/ID3BTL2.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.model_selection import train_test_split 
+from sklearn.datasets import load_iris
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+
+# Đọc dữ liệu và chuẩn bị tập huấn luyện
+df = pd.read_csv('D:/code hoc may/Breast_cancer_data.csv') #đọc tập csv và lưu trữ dạng dataframe
+X_data = np.array(df[['mean_radius','mean_texture','mean_perimeter','mean_area','mean_smoothness','diagnosis']].values) # lấy giá trị của các cột được chọn từ df và chuyển đổi thành mảng numpy
+data=X_data  #gán cho data
+dt_Train, dt_Test = train_test_split(data, test_size=0.3, shuffle = True)
+dt_Train, dt_Test = train_test_split(data, test_size=0.3, shuffle = True)
+np.set_printoptions(suppress=True)
+print(dt_Train)
+
+X_train = dt_Train[:, :5]
+y_train = dt_Train[:, 5]
+X_test = dt_Test[:, :5]
+y_test = dt_Test[:, 5]
+
+# Xây dựng và huấn luyện mô hình ID3 Decision Tree
+id3 = DecisionTreeClassifier(criterion='entropy',splitter='best',min_samples_split=2)
+id3.fit(X_train, y_train)
+
+# Dự đoán kết quả trên tập kiểm tra
+y_pred = id3.predict(X_test)
+
+# Đánh giá độ chính xác
+accuracy = np.mean(y_pred == y_test)
+print('Độ chính xác của ID3 Decision Tree:', accuracy)
